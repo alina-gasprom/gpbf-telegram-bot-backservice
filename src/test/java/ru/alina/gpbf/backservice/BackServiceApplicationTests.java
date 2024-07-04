33849db24fed04fb66b90c3c2fa0a8a316b7fa90
alina-gasprom <alina.k.91@icloud.com>
2024-07-04 10:17:09 +0300

    initial

---
+++ src/test/java/ru/alina/gpbf/backservice/BackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alina.gpbf.backservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
